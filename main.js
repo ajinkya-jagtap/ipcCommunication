859e6db0187d7c0d649b7b3cfd330642fe9f8a75
ajinkya-jagtap <ajinkyaj897@gmail.com>
2020-04-02 11:44:02 +0530

    Handled IPC communication

--- main.js
+++ main.js
@@ -9,15 +9,50 @@ const path = require("path");
 const windowStateKeeper = require("electron-window-state");
 let mainWindow;
 
-//Listen for url coming from Renderer
-ipcMain.on('url-to-main',(e,itemUrl) => {
-  //console.log('url from renderer',itemUrl);
-  readItem(itemUrl,item => {
-    console.log('item from readItem',item)
+ipcMain.on('to-main',(e,args) => {
+  let url = args;
+  console.log('in on-main',url, typeof(url));
 
-    //e.sender.send('from-main-to-renderer',item)
-  })
+  // let win = new BrowserWindow({ width: 800, height: 1500 })
+  // win.loadURL(url);
+
+  // let contents = win.webContents
+  // console.log('contents:',contents)
+
+    offScreenWindow = new BrowserWindow({
+      width:500,
+      height:500,
+      show:false,
+      webPreferences:{
+        offscreen:true,
+        nodeIntegration:false
+      }
+    })
+
+    offScreenWindow.loadURL(url)
+
+
+    offScreenWindow.webContents.on('did-finish-load', (event) => {
+       var title = offScreenWindow.getTitle()
+        console.log('title is:',title);
+        if (e.hasOwnProperty('sender')) {
+          e.sender.send('to-renderer',title)
+          // offScreenWindow.webContents.capturePage(image => {
+          //   let screenShot = image.toDataURL()
+          //   console.log('image ad screeshot: ', image,screenShot);
+          //   offScreenWindow.webContents.send('to-renderer',{title,screenShot,url})
+          //   offScreenWindow.close()
+          //   offScreenWindow=null;
+          // })
+        } else {
+          console.log('e.sender not available:', e);
+        }
+    })
+
+  //e.sender.send('to-renderer', 'Welcome to new window')
 })
+//console.log(url);
+
 
 function createWindow() {
   // Win state keeper
@@ -38,7 +73,10 @@ function createWindow() {
     }
   });
 
-  console.log('mainWindow', mainWindow);
+  // mainWindow.webContents.on('did-finish-load', e => {
+  //   let title = mainWindow.getTitle()
+  // console.log('title is:',title);
+  // })
 
   state.manage(mainWindow);
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { ipcRenderer} from 'electron';
-import { } from 'electron';
+import { ipcRenderer, remote } from 'electron';
+import {} from 'electron';
 //import Fs from 'fs';
-const items = require('./items')
+const items = require('./items');
 
 const electron = (<any>window).require('electron');
 const ipc = electron.ipcRenderer;
@@ -13,66 +13,74 @@ const ipc = electron.ipcRenderer;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'angularElectronReadit';
-
   // Dom Nodes
-showModal = document.getElementById('show-modal')
-closeModal = document.getElementById('close-modal')
-modal = document.getElementById('modal')
-addItem = document.getElementById('add-item')
-itemUrl = document.getElementById('url')
-search = document.getElementById('search')
+  showModal = document.getElementById('show-modal');
+  closeModal = document.getElementById('close-modal');
+  modal = document.getElementById('modal');
+  addItem = document.getElementById('add-item');
+  itemUrl = document.getElementById('url');
+  search = document.getElementById('search');
 
-//Disable and enable modal buttons
- toggleModalButtons = () => {
-   // Check state of buttons
-  //  if (addItem.disabled === true) {
-  //   addItem.disabled = false
-  //   addItem.style.opacity = 1
-  //   addItem.innerText = 'Add Item'
-  //   closeModal.style.display = 'inline'
-  // } else {
-  //   addItem.disabled = true
-  //   addItem.style.opacity = 0.5
-  //   addItem.innerText = 'Adding...'
-  //   closeModal.style.display = 'none'
-  // }
-}
-onAdd(){
-   console.log('+ clicked'),
-   document.getElementById("modal").style.display = "flex";
-   document.getElementById("url").focus();
-  //this.modal.style.display = 'flex'
-}
-onClose(){
-  document.getElementById("modal").style.display = "none";
-  //this.modal.style.display = 'none'
+  //Disable and enable modal buttons
+  toggleModalButtons = () => {
+    // Check state of buttons
+    //  if (addItem.disabled === true) {
+    //   addItem.disabled = false
+    //   addItem.style.opacity = 1
+    //   addItem.innerText = 'Add Item'
+    //   closeModal.style.display = 'inline'
+    // } else {
+    //   addItem.disabled = true
+    //   addItem.style.opacity = 0.5
+    //   addItem.innerText = 'Adding...'
+    //   closeModal.style.display = 'none'
+    // }
+  };
+  onAdd() {
+    console.log('+ clicked'),
+      (document.getElementById('modal').style.display = 'flex');
+    document.getElementById('url').focus();
+    //this.modal.style.display = 'flex'
+  }
+  onClose() {
+    document.getElementById('modal').style.display = 'none';
+    //this.modal.style.display = 'none'
   }
 
-onAddURL(){
-    var value = (document.getElementById("url") as HTMLInputElement).value
-    console.log(value)
+  addToList(item) {
+    let items = document.getElementById('items')
 
-    //send new item url to main process
-    //ipc = electron.ipcRenderer
+    // Create a new DOM node
+    let itemNode = document.createElement('div')
 
-    ipc.on('from-main-to-renderer',(e,newItem) => {
-      console.log(newItem);
- })
+    // Assign "read-item" class
+    itemNode.setAttribute('class', 'read-item')
 
-    ipc.send('url-to-main',value)
-    //this.listenFromMain()
+    // Set item url as data attribute
+    itemNode.setAttribute('data-url', item)
 
+    // Add inner HTML
+    itemNode.innerHTML = `<h2>${item}</h2>`
 
-  //  listenFromMain(){
-  //       //listen for new item from main process
-  //       ipc.on('from-main-to-renderer',(e,newItem) => {
-  //         console.log(newItem);
-  //       })
-   }
+    // Append new node to "items"
+    items.appendChild(itemNode)
+  }
 
-   //listen for new item from main process
-   onSearch(){
+  onAddURL() {
+    console.log('caled addUrl');
 
-   }
+    var value = (document.getElementById('url') as HTMLInputElement).value;
+    console.log(value);
+
+    ipc.send('to-main', value);
+    ipc.once('to-renderer', (e, args) => {
+      // e.preventDefault();
+      console.log('asasasasasasasas: ', e, args, typeof(args));
+      this.addToList(args);
+    });
+  }
+  //listen for new item from main process
+  onSearch() {
+    console.log('in onSearch');
+  }
 }
